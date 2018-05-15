d73abefde6f2193836475b0a962e19b88934618d
Lisa Pichler <lisa@epiccreations.se>
2018-05-15 11:59:14 +0200

    strukturerat i viewModel

--- app/src/main/java/com/videlilja/linda/lia/screen/quiz/QuizActivity.java
+++ app/src/main/java/com/videlilja/linda/lia/screen/quiz/QuizActivity.java
@@ -1,31 +1,30 @@
 package com.videlilja.linda.lia.screen.quiz;
 
-        import android.arch.lifecycle.Observer;
-        import android.arch.lifecycle.ViewModelProviders;
-        import android.content.Context;
-        import android.content.Intent;
-        import android.os.Bundle;
-        import android.support.annotation.Nullable;
-        import android.support.v7.app.AppCompatActivity;
-        import android.support.v7.widget.GridLayoutManager;
-        import android.support.v7.widget.RecyclerView;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.TextView;
-        import com.videlilja.linda.lia.R;
-        import com.videlilja.linda.lia.model.Categories;
-        import com.videlilja.linda.lia.model.Quiz;
-        import com.videlilja.linda.lia.screen.endofgame.EndOfGameActivity;
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Pair;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-        import java.util.List;
+import com.videlilja.linda.lia.R;
+import com.videlilja.linda.lia.model.Categories;
+import com.videlilja.linda.lia.model.Quiz;
+import com.videlilja.linda.lia.screen.endofgame.EndOfGameActivity;
+
+import java.util.List;
 
 
 public class QuizActivity extends AppCompatActivity {
 
-    private int rightAnswers = 0;
-    boolean rightAnswer = false;
-    private int amountA = 0;
-    public boolean perfect = false;
+    private Button mNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,81 +42,45 @@ public class QuizActivity extends AppCompatActivity {
         final TextView questionTxt = findViewById(R.id.question_text);
         questionTxt.setText(viewModel.getmRightAnswer().getmQuestion());
 
-        Button next = findViewById(R.id.next);
-        next.setEnabled(false);
-        next.setBackgroundColor(getResources().getColor(R.color.noColor));
-
         final QuizAdapter adapter = new QuizAdapter();
+        RecyclerView recyclerView = findViewById(R.id.quiz_recycler_view);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        recyclerView.setAdapter(adapter);
+
+        mNext = findViewById(R.id.next);
+        mNext.setEnabled(false);
+        mNext.setBackgroundColor(getResources().getColor(R.color.noColor));
+        mNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (viewModel.rightAnswers < 9) {
+                    viewModel.setmQuiz(category);
+                } else {
+                    // Spelet är slut, byt till EndOfGame
+                    Intent intent = new Intent(getApplicationContext(), EndOfGameActivity.class);
+
+                    //Skicka med prefect och score
+                    Bundle bundle = new Bundle();
+                    bundle.putBoolean("perfect", (viewModel.rightAnswers == viewModel.amountA));
+                    bundle.putString("score", Integer.toString(10 - (viewModel.amountA - viewModel.rightAnswers)));
+                    bundle.putSerializable("action", category);
+                    intent.putExtras(bundle);
+
+                    //Starta nästa activity
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
 
 
         adapter.setmQuizClickListener(new OnQuizClickedListener() {
 
             @Override
             public void onQuizClicked(final Quiz quiz) {
-                if (!rightAnswer) {
-                    if (quiz == viewModel.getmRightAnswer()) {
-                        //RÄTT SVAR
-                        rightAnswer = true;
-                        rightAnswers = rightAnswers + 1;
-                        amountA = amountA + 1;
-                        Button next = findViewById(R.id.next);
-                        next.setEnabled(true);
-                        next.setBackgroundColor(getResources().getColor(R.color.colorSecondary));
-                        // Läs in listan igen med förändrad bakgrund
-                        quiz.setBackgroundColor(R.color.colorGreen);
-
-                        // 0.5 seconds delay
-                        try {
-                            Thread.sleep(500);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-
-                        if (rightAnswers < 10) {
-
-                            next.setOnClickListener(new View.OnClickListener() {
-                                @Override
-                                public void onClick(View view) {
-                                    Button next = findViewById(R.id.next);
-                                    next.setEnabled(false);
-                                    next.setBackgroundColor(getResources().getColor(R.color.noColor));
-                                    viewModel.setmQuiz(category);
-                                }
-                            });
-
-                        } else {
-                            if (rightAnswers == amountA) {
-                                perfect = true;
-                            }
-
-                            next.setOnClickListener(new View.OnClickListener() {
-                                @Override
-                                public void onClick(View view) {
-                                    // Spelet är slut, byt till EndOfGame
-                                    Intent intent = new Intent(getApplicationContext(), EndOfGameActivity.class);
-
-                                    //Skicka med prefect och score
-                                    Bundle bundle = new Bundle();
-                                    bundle.putBoolean("perfect", perfect);
-                                    bundle.putString("score", Integer.toString(10 - (amountA - rightAnswers)));
-                                    bundle.putSerializable("action", category);
-                                    intent.putExtras(bundle);
-
-                                    //Starta nästa activity
-                                    startActivity(intent);
-                                    finish();
-                                }
-                            });
-                        }
-                    } else {
-                        //FEL SVAR
-                        quiz.setBackgroundColor(R.color.colorRed);
-                        rightAnswer = false;
-                        amountA = amountA + 1;
-                        Button next = findViewById(R.id.next);
-                        next.setEnabled(false);
-                        next.setBackgroundColor(getResources().getColor(R.color.noColor));
-                        next.getShadowColor();
+                if (viewModel.rightAnswer.getValue() != null) {
+                    if (!viewModel.rightAnswer.getValue().first) {
+                        viewModel.quizClicked(quiz);
                     }
                 }
             }
@@ -126,17 +89,44 @@ public class QuizActivity extends AppCompatActivity {
         viewModel.getQuiz().observe(this, new Observer<List<Quiz>>() {
             @Override
             public void onChanged(@Nullable final List<Quiz> quizzes) {
-                rightAnswer = false;
+                mNext.setEnabled(false);
+                mNext.setBackgroundColor(getResources().getColor(R.color.noColor));
+                mNext.getShadowColor();
                 adapter.setQuizList(quizzes);
                 sortTxt.setText(viewModel.getmRightAnswer().getmName());
             }
         });
 
-        RecyclerView recyclerView = findViewById(R.id.quiz_recycler_view);
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
-        recyclerView.setAdapter(adapter);
+        viewModel.rightAnswer.observe(this, new Observer<Pair<Boolean, Quiz>>() {
+            @Override
+            public void onChanged(@Nullable Pair<Boolean, Quiz> rightAnswer) {
+                if (rightAnswer != null) {
+
+                    if (rightAnswer.second != null) {
+
+                        if (rightAnswer.first) {
+
+                            mNext.setEnabled(true);
+                            mNext.setBackgroundColor(getResources().getColor(R.color.colorSecondary));
+                            rightAnswer.second.setBackgroundColor(R.color.colorGreen);
+
+                        } else {
+
+                            rightAnswer.second.setBackgroundColor(R.color.colorRed);
+                            mNext.setEnabled(false);
+                            mNext.setBackgroundColor(getResources().getColor(R.color.noColor));
+                            mNext.getShadowColor();
+
+                        }
+
+                    }
+                }
+            }
+        });
+
     }
-    public static void start(Context context, Categories action){
+
+    public static void start(Context context, Categories action) {
         Intent intent = new Intent(context, QuizActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.putExtra("category", action);
--- app/src/main/java/com/videlilja/linda/lia/screen/quiz/QuizViewModel.java
+++ app/src/main/java/com/videlilja/linda/lia/screen/quiz/QuizViewModel.java
@@ -5,14 +5,21 @@ import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.util.Pair;
+import android.view.View;
+import android.widget.Button;
 
+import com.videlilja.linda.lia.R;
 import com.videlilja.linda.lia.model.Categories;
 import com.videlilja.linda.lia.model.CategoriesLiveData;
 import com.videlilja.linda.lia.model.Category;
 import com.videlilja.linda.lia.model.Games;
 import com.videlilja.linda.lia.model.Quiz;
 import com.videlilja.linda.lia.model.QuizLiveData;
+import com.videlilja.linda.lia.screen.endofgame.EndOfGameActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,8 +31,17 @@ public class QuizViewModel extends AndroidViewModel {
     private MutableLiveData<List<Quiz>> mOptions = new MutableLiveData<>();
     private Quiz mRightAnswer;
 
+    public int rightAnswers = 0;
+    public MutableLiveData<Pair<Boolean, Quiz>> rightAnswer = new MutableLiveData<>();
+    public int amountA = 0;
+    public boolean perfect = false;
+
     public QuizViewModel(@NonNull Application application) {
         super(application);
+        rightAnswer.setValue(new Pair<Boolean, Quiz>(false, null));
+        perfect = false;
+        rightAnswers = 0;
+        amountA = 0;
     }
 
     public void setmQuiz(Categories category) {
@@ -33,7 +49,7 @@ public class QuizViewModel extends AndroidViewModel {
         Random random = new Random();
         List<Quiz> options = new ArrayList<>();
 
-        while (options.size() < 4){
+        while (options.size() < 4) {
             int n = random.nextInt(quiz.size());
             if (!options.contains(quiz.get(n))) {
                 options.add(quiz.get(n));
@@ -42,6 +58,7 @@ public class QuizViewModel extends AndroidViewModel {
 
         mRightAnswer = options.get(random.nextInt(options.size()));
         mOptions.setValue(options);
+        rightAnswer.setValue(new Pair<Boolean, Quiz>(false, null));
     }
 
     public Quiz getmRightAnswer() {
@@ -51,4 +68,23 @@ public class QuizViewModel extends AndroidViewModel {
     public LiveData<List<Quiz>> getQuiz() {
         return mOptions;
     }
+
+    public void quizClicked(Quiz quiz) {
+        if (rightAnswer.getValue() != null) {
+            if (!rightAnswer.getValue().first) {
+                if (quiz == getmRightAnswer()) {
+                    //RÄTT SVAR
+                    rightAnswer.setValue(new Pair<>(true, quiz));
+                    rightAnswers = rightAnswers + 1;
+                    amountA = amountA + 1;
+                } else {
+                    //FEL SVAR
+                    rightAnswer.setValue(new Pair<>(false, quiz));
+                    amountA = amountA + 1;
+                }
+            }
+
+        }
+    }
+
 }
